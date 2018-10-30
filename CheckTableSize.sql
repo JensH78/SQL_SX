1bb083d55730517b0ac58f1a8b03fba5a1ef4ebc
JensH78 <43984277+JensH78@users.noreply.github.com>
2018-10-30 11:15:45 +0100

    Add files via upload

---
+++ CheckTableSize.sql
@@ -0,0 +1,38 @@
+/*
+Ermittelt die Anzahl der Datensätze pro Tabelle für einen Mandanten
+*/
+
+DECLARE @stmt AS VARCHAR(MAX)
+      , @CuTableName AS VARCHAR(MAX)
+DECLARE TableCursor cursor FOR
+
+SELECT REPLACE(REPLACE(REPLACE([Name],'/','_'),'.','_'),'%','_')
+FROM dbo.[Object]
+WHERE [Company Name] LIKE '30%'
+
+CREATE TABLE #TableAnalyse
+(        
+    TableName CHAR(2000),
+	Quantity INT     
+);
+
+
+OPEN TableCursor FETCH NEXT FROM TableCursor INTO @CuTableName
+WHILE @@FETCH_STATUS = 0 BEGIN
+SET @stmt =
+'INSERT INTO #TableAnalyse
+ SELECT '''+@CuTableName+''',
+ COUNT(*)
+ FROM [dbo].[30 Simplex Armaturen & Systeme$' + @CuTableName + ']'  
+
+--PRINT(@stmt)
+EXEC(@stmt)
+FETCH NEXT FROM TableCursor INTO @CuTableName
+END
+CLOSE TableCursor
+DEALLOCATE TableCursor
+
+SELECT *
+FROM #TableAnalyse
+ORDER BY [Quantity] DESC
+DROP TABLE #TableAnalyse
